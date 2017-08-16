1fba7a97ab1feb6027486cd68f314461a21bee5a
Wisemonkey <Oranbusiness@gmail.com>
2017-08-15 19:34:57 -0700

    works

---
+++ public/sketch.js
@@ -0,0 +1,18 @@
+/**
+ * Created by wisemonkey on 8/15/2017.
+ */
+
+function setup() {
+
+    createCanvas(400,400);
+    background(50);
+
+    console.log("~Client Loaded~");
+    alert("Client Script Loaded.");
+}
+
+
+function draw() {
+
+
+}
---
+++ server.js
@@ -0,0 +1,22 @@
+/**
+ * Created by wisemonkey on 8/15/2017.
+ */
+require('wise-helper');
+var express = require('express')
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(process.env.PORT || 80);
+
+app.use(express.static('public'));
+
+io.on('connection', function (socket) {
+    socket.emit('connected', {msg: "You're connected!"});
+    socket.on('message', function (data) {
+        console.log(data);
+        socket.broadcast.emit('message', data);
+    });
+});
+
+print("Server Running: Arduino Servo Controller");
